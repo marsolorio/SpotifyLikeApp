cc48ce52fe9f52632192d6d16e9ee24b8175e8c0
MARTIN SOLORIO <85971614+marsolorio@users.noreply.github.com>
2024-10-19 19:19:08 -0700

    Added Gson dependency for JSON support

--- SpotifyLikeApp/src/main/java/com/spotifylikeapp/App.java
+++ SpotifyLikeApp/src/main/java/com/spotifylikeapp/App.java
@@ -1,13 +1,8 @@
 package com.spotifylikeapp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Spotify-Like App!");
+        // This is where you can start implementing the menu system for the app.
     }
 }
